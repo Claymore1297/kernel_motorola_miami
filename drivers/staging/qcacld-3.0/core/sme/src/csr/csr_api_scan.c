OnePlusOSS/android_kernel_oneplus_msm8998

qcacld-3.0: Fix cds_mc_thread getting stuck in infinite loop
A null IE in the bss entry was causing a busyloop in the
cds-mc-thread.

Change-Id: Ia8bc11f7ee31b2be48d6f1fd9cfb50bc6e49c0c3
CRs-Fixed: 2043510

core/sme/src/csr/csr_api_scan.c
@@ -7258,8 +7258,12 @@ void csr_init_occupied_channels_list(tpAniSirGlobal pMac, uint8_t sessionId)
 		/* At this time, pBssDescription->Result.pvIes may be NULL */
 		if (!pIes && !QDF_IS_STATUS_SUCCESS(
 			csr_get_parsed_bss_description_ies(pMac,
-				&pBssDesc->Result.BssDescriptor, &pIes)))
+				&pBssDesc->Result.BssDescriptor, &pIes))) {
+			/* Pick next bss entry before continuing */
+			pEntry = csr_ll_next(&pMac->scan.scanResultList, pEntry,
+				     LL_ACCESS_NOLOCK);
 			continue;
+		}
 		csr_scan_add_to_occupied_channels(pMac, pBssDesc, sessionId,
 				&pMac->scan.occupiedChannels[sessionId], pIes,
 				true);

Claymore1297/kernel_motorola_miami

drivers/staging/qcacld-3.0/core/sme/src/csr/csr_api_scan.c
@@ -5874,8 +5874,12 @@ void csr_init_occupied_channels_list(tpAniSirGlobal mac_ctx,
 		ie_ptr = (tDot11fBeaconIEs *) (bss_desc->Result.pvIes);
 		if (!ie_ptr && !QDF_IS_STATUS_SUCCESS(
 			csr_get_parsed_bss_description_ies(mac_ctx,
-				&bss_desc->Result.BssDescriptor, &ie_ptr)))
+				&bss_desc->Result.BssDescriptor, &ie_ptr))) {
+			/* Pick next bss entry before continuing */
+			scan_entry = csr_ll_next(&scan_list->List, scan_entry,
+				     LL_ACCESS_NOLOCK);
 			continue;
+		}
 		csr_scan_add_to_occupied_channels(mac_ctx, bss_desc, sessionId,
 				&mac_ctx->scan.occupiedChannels[sessionId], ie_ptr,
 				true);