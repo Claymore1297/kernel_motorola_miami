OnePlusOSS/android_kernel_oneplus_msm8998

qcacld-3.0: Introduce configuration parameters for bad RSSI roaming
Introduce the below WCNSS_qcom_cfg.ini items to configure
for bad RSSI roaming. These parameters would be used when
the device is connected to an AP with weak signal and has
to roam to a better AP if it is stationary but has found
some other better AP in the vicinity

roam_bg_scan_bad_rssi_thresh:
If the DUT is connected to an AP with weak signal, then the bad RSSI
threshold will be used as an opportunity to use the scan results
from other scan clients and try to roam if there is a better AP
available in the environment.

roam_bg_scan_client_bitmap:
This bitmap is used to define the client scans that need to be used
by the roaming module to perform a background roaming.

Change-Id: I3b9f737ea389d35f8be173ce83b2237c2375fb88
CRs-Fixed: 2082904

core/hdd/src/wlan_hdd_cfg.c
@@ -3985,6 +3985,21 @@ struct reg_table_entry g_registry_table[] = {
 		CFG_ROAM_DENSE_MIN_APS_MIN,
 		CFG_ROAM_DENSE_MIN_APS_MAX),
 
+	REG_VARIABLE(CFG_ROAM_BG_SCAN_BAD_RSSI_THRESHOLD_NAME,
+		WLAN_PARAM_SignedInteger, struct hdd_config,
+		roam_bg_scan_bad_rssi_thresh,
+		VAR_FLAGS_OPTIONAL | VAR_FLAGS_RANGE_CHECK_ASSUME_DEFAULT,
+		CFG_ROAM_BG_SCAN_BAD_RSSI_THRESHOLD_DEFAULT,
+		CFG_ROAM_BG_SCAN_BAD_RSSI_THRESHOLD_MIN,
+		CFG_ROAM_BG_SCAN_BAD_RSSI_THRESHOLD_MAX),
+
+	REG_VARIABLE(CFG_ROAM_BG_SCAN_CLIENT_BITMAP_NAME, WLAN_PARAM_Integer,
+		struct hdd_config, roam_bg_scan_client_bitmap,
+		VAR_FLAGS_OPTIONAL | VAR_FLAGS_RANGE_CHECK_ASSUME_DEFAULT,
+		CFG_ROAM_BG_SCAN_CLIENT_BITMAP_DEFAULT,
+		CFG_ROAM_BG_SCAN_CLIENT_BITMAP_MIN,
+		CFG_ROAM_BG_SCAN_CLIENT_BITMAP_MAX),
+
 	REG_VARIABLE(CFG_ENABLE_FATAL_EVENT_TRIGGER, WLAN_PARAM_Integer,
 			struct hdd_config, enable_fatal_event,
 			VAR_FLAGS_OPTIONAL |
@@ -6331,6 +6346,12 @@ void hdd_cfg_print(hdd_context_t *pHddCtx)
 		CFG_ROAM_DENSE_MIN_APS,
 		pHddCtx->config->roam_dense_min_aps);
 	hdd_debug("Name = [%s] Value = [%u]",
+		CFG_ROAM_BG_SCAN_BAD_RSSI_THRESHOLD_NAME,
+		pHddCtx->config->roam_bg_scan_bad_rssi_thresh);
+	hdd_debug("Name = [%s] Value = [%u]",
+		CFG_ROAM_BG_SCAN_CLIENT_BITMAP_NAME,
+		pHddCtx->config->roam_bg_scan_client_bitmap);
+	hdd_debug("Name = [%s] Value = [%u]",
 		CFG_MIN_REST_TIME_NAME,
 		pHddCtx->config->min_rest_time_conc);
 	hdd_debug("Name = [%s] Value = [%u]",
@@ -8126,6 +8147,10 @@ QDF_STATUS hdd_set_sme_config(hdd_context_t *pHddCtx)
 			pHddCtx->config->roam_dense_min_aps;
 	smeConfig->csrConfig.roam_dense_traffic_thresh =
 			pHddCtx->config->roam_dense_traffic_thresh;
+	smeConfig->csrConfig.roam_bg_scan_bad_rssi_thresh =
+		pHddCtx->config->roam_bg_scan_bad_rssi_thresh;
+	smeConfig->csrConfig.roam_bg_scan_client_bitmap =
+		pHddCtx->config->roam_bg_scan_client_bitmap;
 	smeConfig->csrConfig.obss_width_interval =
 			pHddCtx->config->obss_width_trigger_interval;
 	smeConfig->csrConfig.obss_active_dwelltime =

Claymore1297/kernel_motorola_miami

drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_cfg.c
@@ -3963,6 +3963,21 @@ struct reg_table_entry g_registry_table[] = {
 		CFG_ROAM_DENSE_MIN_APS_MIN,
 		CFG_ROAM_DENSE_MIN_APS_MAX),
 
+	REG_VARIABLE(CFG_ROAM_BG_SCAN_BAD_RSSI_THRESHOLD_NAME,
+		WLAN_PARAM_SignedInteger, struct hdd_config,
+		roam_bg_scan_bad_rssi_thresh,
+		VAR_FLAGS_OPTIONAL | VAR_FLAGS_RANGE_CHECK_ASSUME_DEFAULT,
+		CFG_ROAM_BG_SCAN_BAD_RSSI_THRESHOLD_DEFAULT,
+		CFG_ROAM_BG_SCAN_BAD_RSSI_THRESHOLD_MIN,
+		CFG_ROAM_BG_SCAN_BAD_RSSI_THRESHOLD_MAX),
+
+	REG_VARIABLE(CFG_ROAM_BG_SCAN_CLIENT_BITMAP_NAME, WLAN_PARAM_Integer,
+		struct hdd_config, roam_bg_scan_client_bitmap,
+		VAR_FLAGS_OPTIONAL | VAR_FLAGS_RANGE_CHECK_ASSUME_DEFAULT,
+		CFG_ROAM_BG_SCAN_CLIENT_BITMAP_DEFAULT,
+		CFG_ROAM_BG_SCAN_CLIENT_BITMAP_MIN,
+		CFG_ROAM_BG_SCAN_CLIENT_BITMAP_MAX),
+
 	REG_VARIABLE(CFG_ENABLE_FATAL_EVENT_TRIGGER, WLAN_PARAM_Integer,
 			struct hdd_config, enable_fatal_event,
 			VAR_FLAGS_OPTIONAL |
@@ -6286,6 +6301,12 @@ void hdd_cfg_print(struct hdd_context *hdd_ctx)
 		CFG_ROAM_DENSE_MIN_APS,
 		hdd_ctx->config->roam_dense_min_aps);
 	hdd_debug("Name = [%s] Value = [%u]",
+		CFG_ROAM_BG_SCAN_BAD_RSSI_THRESHOLD_NAME,
+		hdd_ctx->config->roam_bg_scan_bad_rssi_thresh);
+	hdd_debug("Name = [%s] Value = [%u]",
+		CFG_ROAM_BG_SCAN_CLIENT_BITMAP_NAME,
+		hdd_ctx->config->roam_bg_scan_client_bitmap);
+	hdd_debug("Name = [%s] Value = [%u]",
 		CFG_MIN_REST_TIME_NAME,
 		hdd_ctx->config->min_rest_time_conc);
 	hdd_debug("Name = [%s] Value = [%u]",
@@ -8084,6 +8105,10 @@ QDF_STATUS hdd_set_sme_config(struct hdd_context *hdd_ctx)
 			hdd_ctx->config->roam_dense_min_aps;
 	smeConfig->csrConfig.roam_dense_traffic_thresh =
 			hdd_ctx->config->roam_dense_traffic_thresh;
+	smeConfig->csrConfig.roam_bg_scan_bad_rssi_thresh =
+		hdd_ctx->config->roam_bg_scan_bad_rssi_thresh;
+	smeConfig->csrConfig.roam_bg_scan_client_bitmap =
+		hdd_ctx->config->roam_bg_scan_client_bitmap;
 	smeConfig->csrConfig.obss_width_interval =
 			hdd_ctx->config->obss_width_trigger_interval;
 	smeConfig->csrConfig.obss_active_dwelltime =