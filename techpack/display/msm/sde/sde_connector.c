ef2cfbd742a936a263744986515eebc55afa3adb
houdz1 <houdz1@motorola.com>
2020-05-26 10:20:31 +0800

    sde: limit the backlight minimum brightness as bl_min_level
    PROPAGATED_from (CR)
    
    High-precision brightness will set the brightness level from bl_min_level to
    bl_max_level. If we enable 12bit brightness, in fact, the hardware will not
    tolerate very low brightness, these levels cause the backlight unstable.
    
    So we have to define the minimum brightness by 'qcom,mdss-dsi-bl-min-level'
    that the driver does not set the backlight level below that.
    
    Change-Id: Ifa20a92388c6dab20cf5550868f3f3685815936f
    Reviewed-on: https://gerrit.mot.com/1782945
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Yeqing Wang <wangyq13@motorola.com>
    Reviewed-by: Shanshan Dai <daiss1@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- techpack/display/msm/sde/sde_connector.c
+++ techpack/display/msm/sde/sde_connector.c
@@ -138,6 +138,9 @@ static int sde_backlight_device_update_status(struct backlight_device *bd)
 	if (brightness > c_conn->thermal_max_brightness)
 		brightness = c_conn->thermal_max_brightness;
 
+	if(brightness && brightness < display->panel->bl_config.bl_min_level)
+		brightness = display->panel->bl_config.bl_min_level;
+
 	/* map UI brightness into driver backlight level with rounding */
 	bl_lvl = mult_frac(brightness, bl_max_level, brightness_max_level);
 
