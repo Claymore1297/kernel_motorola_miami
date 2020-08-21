UtsavBalar1231/kernel_xiaomi_sm8250

qcacmn: Add DP INI to tune netdev features at runtime
Add INI to modify the HW checksum capability advertisement.
Add hook to update enable/disable of HW checksum for vdev.

Change-Id: Idd0bad1d39c8411c7b4ef19483aa2c75a756791b
CRs-Fixed: 2732099

wlan_cfg/cfg_dp.h
@@ -854,6 +854,27 @@
 		WLAN_CFG_PKTLOG_BUFFER_SIZE, \
 		CFG_VALUE_OR_DEFAULT, "Packet Log buffer size")
 
+/*
+ * <ini>
+ * legacy_mode_csum_disable - Disable csum offload for legacy 802.11abg modes
+ * @Min: 0
+ * @Max: 1
+ * @Default: 0
+ *
+ * This ini is used to disable HW checksum offload capability for legacy
+ * connections
+ *
+ * Related: gEnableIpTcpUdpChecksumOffload should be enabled
+ *
+ * Usage: Internal
+ *
+ * </ini>
+ */
+
+#define CFG_DP_LEGACY_MODE_CSUM_DISABLE \
+	CFG_INI_BOOL("legacy_mode_csum_disable", false, \
+		     "Enable/Disable legacy mode checksum")
+
 #define CFG_DP \
 		CFG(CFG_DP_HTT_PACKET_TYPE) \
 		CFG(CFG_DP_INT_BATCH_THRESHOLD_OTHER) \
@@ -928,6 +949,6 @@
 		CFG(CFG_DP_RX_MON_PROTOCOL_FLOW_TAG_ENABLE) \
 		CFG(CFG_DP_RXDMA_MONITOR_RX_DROP_THRESHOLD) \
 		CFG(CFG_DP_PKTLOG_BUFFER_SIZE) \
-		CFG(CFG_DP_RX_FISA_ENABLE)
-
+		CFG(CFG_DP_RX_FISA_ENABLE) \
+		CFG(CFG_DP_LEGACY_MODE_CSUM_DISABLE)
 #endif /* _CFG_DP_H_ */

Claymore1297/kernel_motorola_miami

drivers/staging/qca-wifi-host-cmn/wlan_cfg/cfg_dp.h
@@ -940,6 +940,26 @@
 #define CFG_DP_PEER_EXT_STATS \
 		CFG_INI_BOOL("peer_ext_stats", \
 		false, "Peer extended stats")
+/*
+ * <ini>
+ * legacy_mode_csum_disable - Disable csum offload for legacy 802.11abg modes
+ * @Min: 0
+ * @Max: 1
+ * @Default: 0
+ *
+ * This ini is used to disable HW checksum offload capability for legacy
+ * connections
+ *
+ * Related: gEnableIpTcpUdpChecksumOffload should be enabled
+ *
+ * Usage: Internal
+ *
+ * </ini>
+ */
+
+#define CFG_DP_LEGACY_MODE_CSUM_DISABLE \
+	CFG_INI_BOOL("legacy_mode_csum_disable", false, \
+		     "Enable/Disable legacy mode checksum")
 
 #define CFG_DP_RX_BUFF_POOL_ENABLE \
 	CFG_INI_BOOL("dp_rx_buff_prealloc_pool", false, \
@@ -1026,6 +1046,6 @@
 		CFG(CFG_DP_PEER_EXT_STATS) \
 		CFG(CFG_DP_RX_BUFF_POOL_ENABLE) \
 		CFG(CFG_DP_RX_PENDING_HL_THRESHOLD) \
-		CFG(CFG_DP_RX_PENDING_LO_THRESHOLD)
-
+		CFG(CFG_DP_RX_PENDING_LO_THRESHOLD) \
+		CFG(CFG_DP_LEGACY_MODE_CSUM_DISABLE)
 #endif /* _CFG_DP_H_ */