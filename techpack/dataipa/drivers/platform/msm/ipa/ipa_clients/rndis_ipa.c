MotorolaMobilityLLC/kernel-msm

msm: ipa3: Add debug logs to check unregister netdev completion time
Add debug logs to check how long unregister netdev completion taking
time.

Change-Id: I0cd7ec2655d32cfdf9b758f6147628c7fa33f568
Signed-off-by: Piyush Dhyani <pdhyani@codeaurora.org>

drivers/platform/msm/ipa/ipa_clients/rndis_ipa.c
@@ -1389,8 +1389,9 @@ void rndis_ipa_cleanup(void *private)
 	rndis_ipa_debugfs_destroy(rndis_ipa_ctx);
 	RNDIS_IPA_DEBUG("debugfs remove was done\n");
 
+	RNDIS_IPA_DEBUG("RNDIS_IPA netdev unregistered started\n");
 	unregister_netdev(rndis_ipa_ctx->net);
-	RNDIS_IPA_DEBUG("netdev unregistered\n");
+	RNDIS_IPA_DEBUG("RNDIS_IPA netdev unregistered completed\n");
 
 	spin_lock_irqsave(&rndis_ipa_ctx->state_lock, flags);
 	next_state = rndis_ipa_next_state(rndis_ipa_ctx->state,

Claymore1297/kernel_motorola_miami

techpack/dataipa/drivers/platform/msm/ipa/ipa_clients/rndis_ipa.c
@@ -1401,8 +1401,9 @@ void rndis_ipa_cleanup(void *private)
 	rndis_ipa_debugfs_destroy(rndis_ipa_ctx);
 	RNDIS_IPA_DEBUG("debugfs remove was done\n");
 
+	RNDIS_IPA_DEBUG("RNDIS_IPA netdev unregister started\n");
 	unregister_netdev(rndis_ipa_ctx->net);
-	RNDIS_IPA_DEBUG("netdev unregistered\n");
+	RNDIS_IPA_DEBUG("RNDIS_IPA netdev unregister completed\n");
 
 	spin_lock_irqsave(&rndis_ipa_ctx->state_lock, flags);
 	next_state = rndis_ipa_next_state(rndis_ipa_ctx->state,