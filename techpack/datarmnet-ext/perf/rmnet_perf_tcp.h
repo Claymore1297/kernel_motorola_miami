2c1f8d924e4510108f62f157f5b36432547cbd76
Subash Abhinov Kasiviswanathan <quic_subashab@quicinc.com>
2023-06-12 17:35:57 -0600

    perf: Update to I83c28811a10f2f4abb77698f405e228ecc98f08a
    Update to I83c28811a10f2f4abb77698f405e228ecc98f08a
    
    Change-Id: I83c28811a10f2f4abb77698f405e228ecc98f08a
    Signed-off-by: Subash Abhinov Kasiviswanathan <quic_subashab@quicinc.com>

--- techpack/datarmnet-ext/perf/rmnet_perf_tcp.h
+++ techpack/datarmnet-ext/perf/rmnet_perf_tcp.h
@@ -8,9 +8,9 @@
 #ifndef DATARMNET3a73abbc73
 #define DATARMNET3a73abbc73
 #include <linux/skbuff.h>
-void DATARMNET9250714999(struct sk_buff*DATARMNET543491eb0f);void 
-DATARMNET9d574e64b1(struct sk_buff*DATARMNET543491eb0f);void DATARMNET7fa1835595
-(u32 hash,u32 DATARMNET9445bc443c);int DATARMNET7e9995246e(void);void 
-DATARMNET38bb6f2b7a(void);
+extern bool DATARMNETc03b668e4c;void DATARMNET9250714999(struct sk_buff*
+DATARMNET543491eb0f);void DATARMNET9d574e64b1(struct sk_buff*DATARMNET543491eb0f
+);void DATARMNET7fa1835595(u32 hash,u32 DATARMNET9445bc443c);int 
+DATARMNET7e9995246e(void);void DATARMNET38bb6f2b7a(void);
 #endif
 
