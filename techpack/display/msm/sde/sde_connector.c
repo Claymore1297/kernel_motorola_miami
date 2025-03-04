marcost2/android_kernel_motorola_sm6375

treewide: Fix some clang warnings

techpack/display/msm/sde/sde_connector.c
@@ -206,7 +206,7 @@ static int sde_backlight_setup(struct sde_connector *c_conn,
 	struct backlight_properties props;
 	struct dsi_display *display;
 	struct dp_panel *dp_panel;
-	struct dsi_backlight_config *dsi_bl_config;
+	struct dsi_backlight_config *dsi_bl_config = NULL;
 	struct sde_kms *sde_kms;
 	static int display_count;
 	char bl_node_name[BL_NODE_NAME_SIZE];

Claymore1297/kernel_motorola_miami

Change-Id: I7148d167ea546c34dea345df175638d37b40f961

techpack/display/msm/sde/sde_connector.c
@@ -200,7 +200,7 @@ static int sde_backlight_setup(struct sde_connector *c_conn,
 {
 	struct backlight_properties props;
 	struct dsi_display *display;
-	struct dsi_backlight_config *bl_config;
+	struct dsi_backlight_config *bl_config = NULL;
 	struct sde_kms *sde_kms;
 	static int display_count;
 	char bl_node_name[BL_NODE_NAME_SIZE];